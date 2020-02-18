a25ca352d13bb7cd8cf3c5090aed11d852c6faad
Alvaro Armas <aarmas92@outlook.com>
2020-02-18 02:34:20 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -18,7 +18,7 @@ end
 end 
 
 
-email_addresses = "john@doe.com, person@somewhere.org"
+email_addresses = "avi@test.com, arel@test.com"
 parser = EmailAddressParser.new(email_addresses)
  
 parser.parse
